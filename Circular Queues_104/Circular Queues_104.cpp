14d064f01f5d889473d4fa75d16b4e122c327e61
alfinur154 <alfinur27@gmail.com>
2023-06-22 15:28:37 +0700

    membuat atau menampilkan program fornt dan rear

--- Circular Queues_104/Circular Queues_104.cpp
+++ Circular Queues_104/Circular Queues_104.cpp
@@ -69,6 +69,17 @@ public:
             cout << "Queues is empty\n";
             return;
         }
+
+        cout << "\nElements in the queue are...\n";
+
+        //jika FRONT_position <= REAR_position, literasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 
 };
