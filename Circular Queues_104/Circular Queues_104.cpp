14a3f0a7f876ef484f7c96beab0768317275c145
alfinur154 <alfinur27@gmail.com>
2023-06-22 15:32:00 +0700

    membaca program dari FRONT hingga akhir array

--- Circular Queues_104/Circular Queues_104.cpp
+++ Circular Queues_104/Circular Queues_104.cpp
@@ -80,6 +80,13 @@ public:
             }
             cout << endl;
         }
+        else {
+            //jika FRONT_position > REAR_position, literasi dari front hingga akhir array
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
     }
 
 };
